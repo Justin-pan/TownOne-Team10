2093f07a89d20bb520b4330d2064e02366bf3822
Justin Pan <justin.deng.pan@gmail.com>
2023-06-10 10:42:14 -0700

    perk script

--- Assets/Scripts/Perk.cs
+++ Assets/Scripts/Perk.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicPerk : MonoBehaviour
+public class Perk : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
